29336e8539d277fab69da621c0e43591d6094146
Santu Mondal <mondalsantu104@gmail.com>
2025-08-26 01:24:14 +0530

    Add signup Router

--- models/user.js
+++ models/user.js
@@ -1,50 +1,52 @@
-const {Schema, model} = require('mongoose');
-const {createHmac, randomBytes} = require('crypto');
+const { Schema, model } = require("mongoose");
+const { createHmac, randomBytes } = require("crypto");
 
-const userSchema  = new Schema({
-    fullName:{
-        type: String,
-        required: true,
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required: true,
-        unique: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    salt:{
-        type: String,
-        required: true, 
-        unique: true,
+    salt: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type: String,
-        required: true, 
+    password: {
+      type: String,
+      required: true,
     },
-    profileImageURL:{
-        type: String,
-        default: "image/user.png"
+    profileImageURL: {
+      type: String,
+      default: "image/user.png",
     },
-    role:{
-        type: String,
-        enum: ['user', 'admin'],
-        default: 'user' 
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
     },
+  },
+  { timestamps: true }
+);
 
-},{ timestamps: true});
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return;
 
-userSchema.pre('save', function(next){
-    const user = this;
-    if(!user.isModified('password')) return;
+  const salt = randomBytes(16).toString();
 
-    const salt = randomBytes(16).toString('hex');
-    user.salt = salt;
-    const hashpassword = createHmac('sha256', salt)
+  const hashpassword = createHmac("sha256", salt)
     .update(user.password)
-    .digest('hex');
+    .digest("hex");
 
-    this.salt = salt;
-    this.password = hashpassword;
-    next();
+  this.salt = salt;
+  this.password = hashpassword;
+  next();
 });
 
 const User = model("user", userSchema);
